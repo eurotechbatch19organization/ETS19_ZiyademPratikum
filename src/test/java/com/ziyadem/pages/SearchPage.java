a8e3e1fb18647a35f960ca13bd6e7b162c031228
syuleyman <syuleymandouble@gmail.com>
2026-01-20 23:14:11 +0100

    syuleyman180126 nin push etmeden önceki commitidir

--- src/test/java/com/ziyadem/pages/SearchPage.java
+++ src/test/java/com/ziyadem/pages/SearchPage.java
@@ -16,13 +16,13 @@ public class SearchPage extends BasePage {
     private WebElement searchButton;
 
     @FindBy(css = ".products .product .woocommerce-loop-product__title")
-    public List<WebElement> productTitles;
+    private List<WebElement> productTitles;
 
     @FindBy(css = ".search-results")
-    public WebElement searchResultsContainer;
+    private WebElement searchResultsContainer;
 
     @FindBy(css = ".search-results .product")
-    public List<WebElement> resultItems;
+    private List<WebElement> resultItems;
 
     @FindBy(css = "h1.product-title")
     private WebElement productTitle;
@@ -30,10 +30,10 @@ public class SearchPage extends BasePage {
     @FindBy(css = ".search-results .product .woocommerce-loop-product__title")
     private List<WebElement> resultItemTitles;
 
-    private By predictiveItems = By.cssSelector(".autocomplete-suggestions .autocomplete-suggestion");
-
     @FindBy(css = ".message-container.container.medium-text-center")
-    public WebElement noProductsMessage;
+    private WebElement noProductsMessage;
+
+    private By predictiveItems = By.cssSelector(".autocomplete-suggestions .autocomplete-suggestion");
 
     private By predictiveResultsContainer = By.cssSelector(".autocomplete-suggestions");
 
@@ -96,45 +96,59 @@ public class SearchPage extends BasePage {
                 .anyMatch(t -> t.contains(lastSearchedTerm.toLowerCase()));
     }
 
+    /**
+     * Bu metod searchte aratilan ürün girildikten sonra acilan sayfada yer alan ürünlerden ilkinin
+     * adini alir
+     */
     public String getFirstResultProductName() {
-        String name = resultItemTitles.get(0).getText().trim();
-        System.out.println("firstProductName = " + name);
-        return name;
+        return resultItemTitles.get(0).getText().trim();
     }
 
+    /**
+     * Bu metod searchte aratilan ürün girildikten sonra acilan sayfada yer alan ürünlerden ilkine tiklar
+     */
     public void clickFirstResultProduct() {
         resultItems.get(0).click();
     }
 
+    /**
+     * Bu metod searchte aratilan ürün girildikten sonra tiklandiginda sayfanin acilip acilmadigini teyid eder
+     */
     public boolean isProductTitleDisplayed() {
         try {
-            System.out.println("productTitle.getText() = " + productTitle.getText());
             return productTitle.isDisplayed();
         } catch (NoSuchElementException e) {
             return false;
         }
     }
 
+    /**
+     * Bu metod searchte aratilan ürün girildikten sonra tiklandiginda acilan sayfada ilk ürün
+     * tiklandiktan sonra ürünün basligini alir
+     */
     public String getProductTitleText() {
         return productTitle.getText();
     }
 
-    public String getSearchInputValue() {
-        String searchInputValue = searchBox.getAttribute("value");
-        System.out.println("searchInputValue = " + searchInputValue);
-        return searchBox.getAttribute("value").trim();
-    }
-
+    /**
+     * Bu metod searchBox'a olmayan bir ürün girilip aratildiginda ekrana cikan bulunamadi mesajini alir
+     */
     public String getNoProductsMessageText() {
         return noProductsMessage.getText();
     }
 
+    /**
+     * Bu metod searchBox a girilen üründen sonra Enter'a basilmasini saglar
+     */
     public void pressEnterOnSearchBar() {
         searchBox.sendKeys(Keys.ENTER);
     }
 
+    /**
+     * Bu metod searchBox'ta en az 3 harflik kelime girildikten sonra acilan dropdown listesinin
+     * acilip acilmadigini teyid eder
+     */
     public boolean isPredictiveDropdownDisplayed() {
-
         try {
             return BrowserUtils.waitForVisibility(predictiveResultsContainer, 5).isDisplayed();
         } catch (TimeoutException e) {
@@ -142,9 +156,10 @@ public class SearchPage extends BasePage {
         }
     }
 
-
+    /**
+     * bu metod searchBox'ta acilan dropdown listesinin eleman sayisinin 0 dan büyük oldugunu döner
+     */
     public int getPredictiveItemsCount() {
-
         try {
             BrowserUtils.waitForVisibility(predictiveResultsContainer, 5);
         } catch (TimeoutException e) {
@@ -153,16 +168,25 @@ public class SearchPage extends BasePage {
         return Driver.get().findElements(predictiveResultsContainer).size();
     }
 
+    /**
+     * bu metod searchBox ta acilan dropdown listesinin ilk ürünün textini alir
+     */
     public String getFirstPredictiveItemText() {
         String raw = Driver.get().findElements(predictiveItems).get(0).getText().trim();
         return raw.split("\\R")[0].trim();
     }
 
+    /**
+     * bu metod searchBox ta acilan dropdown listesinin ilk ürününe tiklar
+     */
     public void clickFirstPredictiveItem() {
         List<WebElement> items = Driver.get().findElements(predictiveItems);
         items.get(0).click();
     }
 
+    /**
+     * bu metod searchBox ta acilan dropdown listesi icin bekleme yapar
+     */
     public void waitForPredictiveDropdown() {
         BrowserUtils.waitForVisibility(predictiveResultsContainer, 5);
     }
